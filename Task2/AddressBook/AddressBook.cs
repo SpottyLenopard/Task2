338e0390252fc3bf2ccd08ce70624a567f72d64f
SpottyLenopard <spotty.lenopard@gmail.com>
2016-05-31 23:44:35 +0300

    task3

--- Task2/AddressBook/AddressBook.cs
+++ Task2/AddressBook/AddressBook.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logger;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -45,5 +46,99 @@ namespace AddressBooks
 
         }
 
+        public List<User> GetUsersWithGmailAccount()
+        {
+            List<User> gmailUsers = new List<User>();
+            if (_addressBookList.Count > 0)
+                gmailUsers = _addressBookList.Where(x => x.Email.ToUpperInvariant().Contains("GMAIL.COM")).ToList<User>();
+
+            return gmailUsers;
+        }
+
+        public List<User> GetRecentlyAddedWomen()
+        {
+            var res = from user in _addressBookList
+                      where user.Gender == 'F' && user.DateAdded > DateTime.Today.AddDays(-10)
+                      select user;
+
+            return res != null ? res.ToList<User>() : new List<User>();             
+        }
+
+        public List<User> GetJanuaryBornUsersWithPhoneAndAddress()
+        {
+            List<User> januaryBornUsersWithPhoneAndAddress = new List<User>();
+            if (_addressBookList.Count > 0)
+                januaryBornUsersWithPhoneAndAddress = _addressBookList.Where(x => x.BirthDate.Month==1 &&
+                                                                                    x.Address != string.Empty &&
+                                                                                    x.PhoneNumber != string.Empty).OrderByDescending(x => x.LastName).ToList<User>();
+
+            return januaryBornUsersWithPhoneAndAddress;
+        }
+
+        public Dictionary<string, List<User>> GetUserDictionaryByGender()
+        {
+            try
+            {
+                var genderDictionary = new Dictionary<string, List<User>>
+                {
+                    {"man",_addressBookList.Where(x => x.Gender =='M').ToList<User>() },
+                    {"woman",_addressBookList.Where(x => x.Gender =='F').ToList<User>() }
+                };
+
+                return genderDictionary;
+            }
+            catch (Exception)
+            {
+                LoggerFactory.GetLogger().Error("can not create dictionary");
+            }
+
+            return new Dictionary<string, List<User>>();
+        }
+
+        public List<User> GetFilteredUsers(Func<User, bool> condition, int from, int to)
+        {
+            List<User> filteredUsers = new List<User>();
+
+            if (_addressBookList.Count > 0)
+                try
+                {
+                    filteredUsers = _addressBookList.Where(condition).Skip(from - 1).Take(to - from).ToList<User>();
+                }
+                catch
+                {
+                    LoggerFactory.GetLogger().Error("Unable to select requested range");
+                }
+
+            return filteredUsers;
+        }
+
+        public List<User> GerUsersFromCertainCity(string city)
+        {
+            var res = from user in _addressBookList
+                      where user.City == city && user.BirthDate.Month == DateTime.Today.Month &&
+                      user.BirthDate.Day == DateTime.Today.Day
+                      select user;
+
+            return res != null ? res.ToList<User>() : new List<User>();
+        }
+
+        public List<User> GetAddressBookList()
+        {
+            return _addressBookList;
+        }
+
+
     }
+
+    public static class AddressBookExtensions
+    {
+        public static IEnumerable<User> GetKyivAdultUsers(this AddressBook ad)
+        {
+            foreach (User u in ad.GetAddressBookList())
+                if (u.BirthDate != null && u.City.ToUpperInvariant() != "KIEV" && u.BirthDate < DateTime.Now.Date.AddYears(-18))
+                    yield return u;
+
+        }
+    }
+
 }
--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -28,7 +28,7 @@ namespace Task2
             addressBook.UsedAdded += handler.LogAddUserChange;
             addressBook.UserRemoved += handler.LogRemoveUserChange;
             
-            User addedUser = new User("Ivanov", "Ivan", DateTime.Parse("2010/10/02"), "Kyiv", "Gonchara 23", "87324232", 'M', "i.ivanov@gmail.com");
+            User addedUser = new User("Ivanov", "Ivan", DateTime.Parse("2010/10/02"), "Kiev", "Gonchara 23", "87324232", 'M', "i.ivanov@gmail.com");
 
             try
             {
@@ -41,7 +41,7 @@ namespace Task2
             }
 
 
-            User removedUser = new User("Petrov", "Ivan", DateTime.Parse("2010/10/02"), "Kyiv", "Gonchara 23", "87324232", 'M', "i.petrov@gmail.com");
+            User removedUser = new User("Petrov", "Ivan", DateTime.Parse("2010/10/02"), "Kiev", "Gonchara 23", "87324232", 'M', "i.petrov@gmail.com");
 
             try
             { 
@@ -83,8 +83,100 @@ namespace Task2
                 fileLogger.Error(ex.Message);
             }
 
+            FillAddressBook(addressBook);
+            ShowUsersWithGoogleAccount(addressBook, consoleLogger);
+            consoleLogger.Debug("\n");
+            ShowAdultsFromKiev(addressBook, consoleLogger);
+            consoleLogger.Debug("\n");
+            ShowRecentlyAddedWomen(addressBook, consoleLogger);
+            consoleLogger.Debug("\n");
+            ShowJanuaryBorns(addressBook, consoleLogger);
+            consoleLogger.Debug("\n");
+            ShowGenderDic(addressBook, consoleLogger);
+            consoleLogger.Debug("\n");
+            ShowFilteredUsers(addressBook, consoleLogger);
+            consoleLogger.Debug("\n");
+            ShowTodayBithDayUsers(addressBook, consoleLogger);
             Console.ReadLine();
 
+        }
+
+        public static void ShowUsersWithGoogleAccount(AddressBook ad, ILogger log)
+        {
+            log.Info("Gmail users:");
+            if (ad != null)
+                foreach (User u in ad.GetUsersWithGmailAccount())
+                    log.Info(u.ToString());
+                
+        }
+
+        public static void ShowAdultsFromKiev(AddressBook ad, ILogger log)
+        {
+            log.Info("Kiev Adults");
+            if (ad != null)
+                foreach (User u in ad.GetKyivAdultUsers())
+                    log.Info(u.ToString());
+        }
+
+        public static void ShowRecentlyAddedWomen(AddressBook ad, ILogger log)
+        {
+            log.Info("Recently added women");
+            if (ad != null)
+                foreach (User u in ad.GetRecentlyAddedWomen())
+                    log.Info(u.ToString());
+        }
+
+        public static void ShowJanuaryBorns(AddressBook ad, ILogger log)
+        {
+            log.Info("January born people");
+            if (ad != null)
+                foreach (User u in ad.GetJanuaryBornUsersWithPhoneAndAddress())
+                    log.Info(u.ToString());
+        }
+
+        public static void ShowGenderDic(AddressBook ad, ILogger log)
+        {
+            log.Info("people by gender");
+            if (ad != null)
+            {
+                foreach (User u in ad.GetUserDictionaryByGender()["man"])
+                    log.Info(u.ToString());
+                log.Info("next");
+                foreach (User u in ad.GetUserDictionaryByGender()["woman"])
+                    log.Info(u.ToString());
+            }
+        }
+
+        public static void ShowFilteredUsers(AddressBook ad, ILogger log)
+        {
+            log.Info("filerted users");
+            if (ad != null)
+            {
+                foreach (User u in ad.GetFilteredUsers(u =>u.City.ToUpperInvariant() == "KIEV",2,5))
+                    log.Info(u.ToString());
+
+            }
+        }
+
+        public static void ShowTodayBithDayUsers(AddressBook ad, ILogger log)
+        {
+            log.Info("today bithday users");
+            if (ad != null)
+            {
+                foreach (User u in ad.GerUsersFromCertainCity("Dnepr")) 
+                    log.Info(u.ToString());
+
+            }
+        }
+
+        public static void FillAddressBook(AddressBook ad)
+        {
+            ad.AddUser(new User("Sidorova", "Svetlana", DateTime.Parse("1987/10/02"), "Kiev", "Gonchara 25", "87324232", 'F', "s.sid@gmail.com"));
+            ad.AddUser(new User("Efimova", "Keti", DateTime.Parse("1967/10/02"), "Dnepr", "Gonchara 25", "87324232", 'F', "k.efi@gmail.com"));
+            ad.AddUser(new User("Romov", "Stepan", DateTime.Parse("1967/01/02"), "Dnepr", "Gonchara 25", "87324232", 'M', "s.romov@gmail.com"));
+            ad.AddUser(new User("Romova", "Lyuba", DateTime.Parse("1967/01/02"), "Dnepr", "", "87324232", 'F', "l.romova@mail.ru"));
+            ad.AddUser(new User("Last", "Klava", DateTime.Parse("1967/01/02"), "Dnepr", "Ivana", "87324232", 'F', "k.Last@mail.ru"));
+            ad.AddUser(new User("Lastov", "Klavdiy", DateTime.Parse("1967/05/31"), "Dnepr", "Ivana", "87324232", 'M', "k.Lastov@mail.ru"));
         }
     }
 }
